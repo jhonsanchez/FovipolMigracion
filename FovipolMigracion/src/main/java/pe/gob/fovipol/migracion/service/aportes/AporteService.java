5e4ef3a3a82455c9241246800a296f67cc4b1ab7
Luis Bellido <luis.bellido2010@gmail.com>
2014-10-25 09:07:19 -0500

    confirmacion

--- FovipolMigracion/src/main/java/pe/gob/fovipol/migracion/service/aportes/AporteService.java
+++
@@ -1,9 +0,0 @@
-package pe.gob.fovipol.migracion.service.aportes;
-
-import java.util.List;
-
-import pe.gob.fovipol.migracion.model.Aporte;
-
-public interface AporteService {
-	public List<Aporte>getAporteBen(String P_C_C_CLIENTE,String tipo);
-}
